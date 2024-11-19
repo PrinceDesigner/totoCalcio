9af508a7084513e59317bc606742aed4d365007c
Michele Errico <maik99@me.com>
2024-11-19 10:50:25 +0100

    concesso tracciamento

--- totoCalcioReact/screens/OnboardingScreen.js
+++ totoCalcioReact/screens/OnboardingScreen.js
@@ -1,26 +1,49 @@
 // screens/OnboardingScreen.js
 
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet, Image } from 'react-native';
-import { Button, Text, useTheme } from 'react-native-paper';
+import { Button, Text, useTheme,use } from 'react-native-paper';
+import { requestTrackingPermissionsAsync } from 'expo-tracking-transparency';
+import { COLORJS } from '../theme/themeColor';
+import fontStyle from '../theme/fontStyle';
+
 
 export default function OnboardingScreen({ navigation }) {
     const { colors } = useTheme(); // Recupera i colori dal tema
 
+    useEffect(() => {
+        const askPermission = async () => {
+          const { status } = await requestTrackingPermissionsAsync();
+          if (status === 'granted') {
+            console.log('Permesso di tracciamento concesso.');
+            // Puoi procedere con il tracciamento
+          } else {
+            console.log('Permesso di tracciamento negato.');
+            // Gestisci il caso in cui il permesso è negato
+          }
+        };
+        askPermission();
+      }, []); // Chiede il permesso al caricamento dell'app
+
     return (
-        <View style={{ ...styles.container, backgroundColor: colors.background }}>
+        <View style={{ ...styles.container, backgroundColor: '#00f310' }}>
             <Image
-                source={require('../image.png')} // Path relativo al file image.png
+                source={require('../league1.png')} // Path relativo al file image.png
                 style={styles.logo} // Applica gli stili
             />
-            <Text style={styles.title}>Benvenuto nell'app Totocalcio!</Text>
+            <Text style={styles.title}>Benvenuto!</Text>
             <Text style={styles.description}>
-                Unisciti a leghe e fai il tuo pronostico sulle partite settimanali.
+                Crea/Unisciti una/ad leghe, invita amici, fai il tuo pronostico sulle partite settimanali. E arriva in cima alla classifica
             </Text>
             <Button
                 mode="contained"
                 onPress={() => navigation.navigate('LoginScreen')}
                 style={styles.joinButton}
+                labelStyle={{
+                    color: 'white',
+                    ...fontStyle.textBold
+
+                }}
             >
                 Join
             </Button>
@@ -38,31 +61,33 @@ const styles = StyleSheet.create({
         width: 300,  // Larghezza dell'immagine
         height: 300, // Altezza dell'immagine
         marginBottom: 20, // Spazio sotto l'immagine
+        backgroundColor: '#00f310'
     },
     title: {
         fontSize: 24,
-        fontWeight: 'bold',
         marginBottom: 10,
-        textAlign: 'center'
+        textAlign: 'center',
+        color: COLORJS.secondaryBackGroud,
+        ...fontStyle.textBold
+        
     },
     description: {
         fontSize: 13,
         textAlign: 'center',
-        marginBottom: 20,
-        fontFamily: "Source Sans Pro"
+        color: COLORJS.secondaryBackGroud,
+        ...fontStyle.textMedium
 
     },
     joinButton: {
-        marginTop: 80,
+        marginTop: 30,
         width: '100%',
         height: 49,
         border: '0',
         borderRadius: 24,
         fontSize: 16,
-        fontWeight: 500,
-        fontFamily: 'Helvetica',
         lineHeight: 21,
         display: 'flex',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundColor: COLORJS.background,
     },
 });
