62efa304770f1268b35252d659f490a627136ed8
Michele Errico <maik99@me.com>
2024-11-21 12:19:50 +0100

    adjust image

--- totoCalcioReact/screens/componentScreen/ProfilCard.js
+++ totoCalcioReact/screens/componentScreen/ProfilCard.js
@@ -1,41 +1,59 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Modal, Image, TouchableOpacity } from 'react-native';
 import { Avatar, Button } from 'react-native-paper';
-import { useSelector } from 'react-redux'; // Importa useSelector
 import fontStyle from '../../theme/fontStyle';
 import { COLORJS } from '../../theme/themeColor';
 
 const ProfileCard = ({ onAvatarPress, fullName, photoProfile }) => {
-    // Utilizza useSelector per ottenere le informazioni dell'utente dallo store Redux
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const handleLongPress = () => {
+        setIsModalVisible(true); // Mostra il modal
+    };
+
+    const closeModal = () => {
+        setIsModalVisible(false); // Chiudi il modal
+    };
 
     return (
         <View style={styles.profileSectionRow}>
             <View style={styles.avatarContainer}>
-                {onAvatarPress ? (
-                    // Se onAvatarPress è definito, avvolgi l'avatar in un TouchableOpacity per renderlo cliccabile
-                    <TouchableOpacity onPress={onAvatarPress}>
-                        <Avatar.Image
-                            size={80}
-                            source={photoProfile ? { uri: photoProfile } : require('../../User-avatar.svg.png') }
-                            style={styles.avatar}
-                        />
-                    </TouchableOpacity>
-                ) : (
-                    // Se onAvatarPress non è definito, mostra solo l'avatar senza interazione
+                <TouchableOpacity
+                    onPress={onAvatarPress ? onAvatarPress: handleLongPress } // Funzionalità già esistente per il click
+                    onLongPress={handleLongPress} // Aggiungi l'azione per il long press
+                >
                     <Avatar.Image
                         size={80}
-                        source={photoProfile ? { uri: photoProfile } : require('../../User-avatar.svg.png') }
+                        source={photoProfile ? { uri: photoProfile } : require('../../User-avatar.svg.png')}
                         style={styles.avatar}
                     />
-                )}
+                </TouchableOpacity>
             </View>
             <View style={styles.profileInfoContainer}>
                 <Text style={styles.userNameText}>{fullName}</Text>
-                <Button style={styles.inviteButton}>
-                    Invita amici
-                </Button>
+                <Button style={styles.inviteButton}>Invita amici</Button>
             </View>
+
+            {/* Modal per l'immagine fullscreen */}
+            <Modal
+                transparent={true}
+                visible={isModalVisible}
+                onRequestClose={closeModal}
+                animationType="fade"
+            >
+                <View style={styles.modalContainer}>
+                    <TouchableOpacity style={styles.modalBackdrop} onPress={closeModal} />
+                    <View style={styles.modalContent}>
+                        <Image
+                            source={photoProfile ? { uri: photoProfile } : require('../../User-avatar.svg.png')}
+                            style={styles.fullscreenImage}
+                        />
+                        <Button onPress={closeModal} mode="contained" style={styles.closeButton}>
+                            Chiudi
+                        </Button>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 };
@@ -56,10 +74,8 @@ const styles = StyleSheet.create({
     avatar: {
         marginRight: 20,
         borderWidth: 2,
-        borderColor: COLORJS.background
-
+        borderColor: COLORJS.background,
     },
-
     userNameText: {
         fontSize: 20,
         ...fontStyle.textBold,
@@ -70,7 +86,36 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         color: COLORJS.primary,
         ...fontStyle.textBold,
-      },
+    },
+    modalContainer: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalBackdrop: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+    modalContent: {
+        width: '90%',
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    fullscreenImage: {
+        width: '100%',
+        height: 300,
+        resizeMode: 'contain',
+    },
+    closeButton: {
+        marginTop: 20,
+        backgroundColor: COLORJS.primary,
+    },
 });
 
 export default ProfileCard;
