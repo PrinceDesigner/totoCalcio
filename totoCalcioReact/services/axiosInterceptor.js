cc8b915727d580e1125ddab173d5a76141f7691b
Michele Errico <maik99@me.com>
2024-11-30 17:15:47 +0100

    redirect no logout

--- totoCalcioReact/services/axiosInterceptor.js
+++ totoCalcioReact/services/axiosInterceptor.js
@@ -1,10 +1,23 @@
 import axios from 'axios';
 import { getToken } from '../AsyncStorage/AsyncStorage';
+import { logout } from '../redux/slice/authSlice';
+import { removeToken } from '../AsyncStorage/AsyncStorage';
+
+
+
+let storeDispatch = null; // Variabile per memorizzare il dispatch
+let navigator = null; // Per memorizzare la navigazione
+
+// Funzione per configurare il dispatch di Redux
+export const configureAxios = (dispatch, navigationRef) => {
+  storeDispatch = dispatch;
+  navigator = navigationRef;
+};
 
 // Configura l'istanza di Axios
 const axiosInstance = axios.create({
-  // baseURL: 'https://totocalcioreact.uc.r.appspot.com/api', // Imposta il tuo URL backend // testata e funzionante
-  baseURL: 'http://192.168.1.107:5001/api', // Imposta il tuo URL backend
+  baseURL: 'https://totocalcioreact.uc.r.appspot.com/api', // Imposta il tuo URL backend // testata e funzionante
+  // baseURL: 'http://192.168.1.5:5001/api', // Imposta il tuo URL backend
 });
 
 // Aggiungi un interceptor per includere automaticamente il token JWT
@@ -21,4 +34,32 @@ axiosInstance.interceptors.request.use(
   }
 );
 
+
+// Aggiungi un interceptor per gestire errori
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const status = error.response?.status;
+
+    if ((status === 401 || status === 403) && storeDispatch && navigator) {
+      // Dispatch del logout
+      await removeToken();
+      storeDispatch(logout());
+      navigator.navigate('LoginScreen');
+    }
+    return Promise.reject(error);
+  }
+);
+
+
+
 export default axiosInstance;
+
+
+
+
+
+// await removeToken(); 
+
+// // Dispatch del logout per aggiornare lo stato di Redux
+// dispatch(logout());
