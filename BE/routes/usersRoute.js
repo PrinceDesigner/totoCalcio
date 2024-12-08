3502dd55d82eddce13aa84353d07aa6dad233170
Michele Errico <maik99@me.com>
2024-12-08 20:45:29 +0100

    uodate user migrate

--- BE/routes/usersRoute.js
+++ BE/routes/usersRoute.js
@@ -2,8 +2,22 @@ const express = require('express');
 const { getAuth } = require('firebase-admin/auth'); // Firebase Admin SDK per aggiornare l'utente
 const { firestore } = require('../firebaseAdmin'); // Assicurati di aver configurato Firebase Admin SDK
 const authMiddleware = require('../middlewares/authMiddleware'); // Middleware di autenticazione
+const supabase = require('../superBaseConnect');
 
 const router = express.Router();
+
+async function update_user(uuid,displaName,email) {
+  const { data, error } = await supabase
+  .rpc('update_user', { p_uid:uuid,p_displayname:displaName,p_email:email})
+
+
+  if (error) {
+    console.error('Error fetching data:', error);
+  } else {
+    console.log('tutto ok', data)
+    return data
+  }
+}
 
 // Route per aggiornare email e displayName
 router.put('/update-user', authMiddleware, async (req, res) => {
@@ -17,21 +31,10 @@ router.put('/update-user', authMiddleware, async (req, res) => {
       // 1. Aggiorna Firebase Authentication
       const auth = getAuth(); // Ottieni l'istanza di Firebase Auth
       await auth.updateUser(userId, { email, displayName });
-      
-      // 2. Aggiorna la raccolta `users` su Firestore
-      const userRef = firestore.collection('users').doc(userId); // Referenza al documento utente su Firestore
-      await userRef.update({ email, displayName });
-  
-      // 3. Recupera il documento aggiornato
-      const updatedUserDoc = await userRef.get();
-  
-      if (!updatedUserDoc.exists) {
-        return res.status(404).json({ message: 'Utente non trovato.' });
-      }
-  
-      // Restituisci il documento aggiornato
-      const updatedUserData = updatedUserDoc.data();
-      res.status(200).json({ message: 'Email e displayName aggiornati con successo!', user: updatedUserData });
+
+      const response = await update_user(userId, displayName, email);
+
+      res.status(200).json({ message: 'Email e displayName aggiornati con successo!', user: response });
     } catch (error) {
       console.error('Errore durante l\'aggiornamento dell\'utente:', error);
       return res.status(500).json({ message: 'Errore durante l\'aggiornamento dell\'utente.' });
