1ea4f354db9506a75cd192af9de0a0191abb603d
Michele Errico <maik99@me.com>
2024-12-11 14:45:25 +0100

    gestione notifiche per utenti già iscritti

--- BE/routes/usersRoute.js
+++ BE/routes/usersRoute.js
@@ -7,37 +7,37 @@ const router = express.Router();
 
 // Route per aggiornare email e displayName
 router.put('/update-user', authMiddleware, async (req, res) => {
-    const { email, displayName, userId } = req.body; // Prendi email e displayName dal body della richiesta
-  
-    if (!email || !displayName) {
-      return res.status(400).json({ message: 'Email e displayName sono obbligatori.' });
+  const { email, displayName, userId } = req.body; // Prendi email e displayName dal body della richiesta
+
+  if (!email || !displayName) {
+    return res.status(400).json({ message: 'Email e displayName sono obbligatori.' });
+  }
+
+  try {
+    // 1. Aggiorna Firebase Authentication
+    const auth = getAuth(); // Ottieni l'istanza di Firebase Auth
+    await auth.updateUser(userId, { email, displayName });
+
+    // 2. Aggiorna la raccolta `users` su Firestore
+    const userRef = firestore.collection('users').doc(userId); // Referenza al documento utente su Firestore
+    await userRef.update({ email, displayName });
+
+    // 3. Recupera il documento aggiornato
+    const updatedUserDoc = await userRef.get();
+
+    if (!updatedUserDoc.exists) {
+      return res.status(404).json({ message: 'Utente non trovato.' });
     }
-  
-    try {
-      // 1. Aggiorna Firebase Authentication
-      const auth = getAuth(); // Ottieni l'istanza di Firebase Auth
-      await auth.updateUser(userId, { email, displayName });
-      
-      // 2. Aggiorna la raccolta `users` su Firestore
-      const userRef = firestore.collection('users').doc(userId); // Referenza al documento utente su Firestore
-      await userRef.update({ email, displayName });
-  
-      // 3. Recupera il documento aggiornato
-      const updatedUserDoc = await userRef.get();
-  
-      if (!updatedUserDoc.exists) {
-        return res.status(404).json({ message: 'Utente non trovato.' });
-      }
-  
-      // Restituisci il documento aggiornato
-      const updatedUserData = updatedUserDoc.data();
-      res.status(200).json({ message: 'Email e displayName aggiornati con successo!', user: updatedUserData });
-    } catch (error) {
-      console.error('Errore durante l\'aggiornamento dell\'utente:', error);
-      return res.status(500).json({ message: 'Errore durante l\'aggiornamento dell\'utente.' });
-    }
-  });
-  
+
+    // Restituisci il documento aggiornato
+    const updatedUserData = updatedUserDoc.data();
+    res.status(200).json({ message: 'Email e displayName aggiornati con successo!', user: updatedUserData });
+  } catch (error) {
+    console.error('Errore durante l\'aggiornamento dell\'utente:', error);
+    return res.status(500).json({ message: 'Errore durante l\'aggiornamento dell\'utente.' });
+  }
+});
+
 
 // Route per ottenere i displayName, userId, photoURL, punti e leagueId per un array di userId
 router.post('/users-info', async (req, res) => {
@@ -126,7 +126,41 @@ router.post('/save-push-token', async (req, res) => {
   }
 });
 
-  
+
+// Endpoint per verificare se il token di notifica push dell'utente è già salvato
+router.post('/verify-push-token', async (req, res) => {
+  const { userId, expoPushToken } = req.body;
+
+  // Controlla che userId ed expoPushToken siano presenti
+  if (!userId || !expoPushToken) {
+    return res.status(400).json({ error: 'userId e expoPushToken sono richiesti' });
+  }
+
+  try {
+    // Ottieni la referenza al documento utente su Firestore
+    const userRef = firestore.collection('users').doc(userId);
+    const userDoc = await userRef.get();
+
+    if (!userDoc.exists) {
+      return res.status(404).json({ error: 'Utente non trovato.' });
+    }
+
+    // Controlla se il token coincide
+    const userData = userDoc.data();
+    const savedToken = userData?.tokenNotification;
+
+    if (savedToken === expoPushToken) {
+      return res.status(200).json({ isTokenValid: true });
+    } else {
+      return res.status(200).json({ isTokenValid: false });
+    }
+  } catch (error) {
+    console.error('Errore durante la verifica del token nel database:', error);
+    return res.status(500).json({ message: 'Errore durante la verifica del token nel database.' });
+  }
+});
+
+
 
 
 
