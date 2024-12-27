752599e018ee34979f325e8a41571c11ed49a190
Michele <maik99@me.com>
2024-12-27 17:52:29 +0100

    Aggiunta funzione per il passaggio da una giornata all'altra schedulata su GCP

--- BE/functions/index.js
+++ BE/functions/index.js
@@ -14,7 +14,7 @@ const { migrateLeaguesAndMembers } = require('./migr_League_memersInfo');
 const {migrationPredictionsAndSchedina } = require('./migr_Predictions_Schedina');
 const { migrateGiornateCalcolate } = require('./migr_Giornate_Calcolate');
 const {calcolaPuntiGiornataTest,calcolaPuntiGiornataTest2 } = require('./calcoloGiornata');
-const {updateMatchesSupa,updateDateMatchSupa } = require('./updateMatch');
+const {updateMatchesSupa,updateDateMatchSupa,scheduleDayUpdateTasks_Supa } = require('./updateMatch');
 const {sendWeeklyNotificationSupa} = require('./notifche');
 const {exportAllTables} = require('./backupTable');
 
@@ -870,6 +870,8 @@ exports.calcolaPuntiGiornataTest2            = calcolaPuntiGiornataTest2; //*
 
 exports.updateMatchesSupa                    = updateMatchesSupa;
 exports.updateDateMatchSupa                  = updateDateMatchSupa;
+exports.scheduleDayUpdateTasks_Supa          = scheduleDayUpdateTasks_Supa;
 
 exports.sendWeeklyNotificationSupa           = sendWeeklyNotificationSupa;
 exports.exportAllTables                      = exportAllTables;
+
